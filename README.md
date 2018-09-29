9331fa0ae52f8f7bb02b45923c370010580f2ac2
Chetan <clakhanpal@deloitte.com>
2018-09-29 23:36:41 +0530

    changes requested

---
+++ README.md
@@ -0,0 +1,19 @@
+# Memory Game
+
+
+Game which encourages player to remember a particular card's position, where user can select at most 2 card to see if they match or not, If it is then there's a match, or else current cards will be flipped again.
+
+## Installation
+`git clone https://github.com/chetanlakhanpal/memorygame`
+
+To start, Open index.html
+
+
+## Libraries Used
+
+Font awesome
+Bootstrap
+
+
+#### About
+Created by Chetan Lakhanpal
--- js/app.js
+++ js/app.js
@@ -2,18 +2,13 @@ function MemoryGame() {
   this.cards = [];
   this.moves;
   this.matchCount;
-  this.threeStarLimit = 8;
-  this.twoStarLimit = 12;
   this.starLimit = 0;
   this.lastSelectedCard;
   this.timeoutCounter;
   this.container = document.querySelector(".deck");
   this.movesElement = document.querySelector(".moves");
   this.starsContainer = document.querySelector(".stars");
-  this.starElement = this.starsContainer.children[0];
-  this.starElements = `<li><i class="fa fa-star"></i></li>
-        		<li><i class="fa fa-star"></i></li>
-        		<li><i class="fa fa-star"></i></li>`;
+  this.starElement = document.querySelector('.stars > li');
   this.preventClick = false;
   this.modal = document.querySelector("#cong-modal");
   this.modalbackDrop = document.querySelector("#backdrop");
@@ -21,6 +16,7 @@ function MemoryGame() {
   this.intervalHandler;
   this.timerElement = document.querySelector("#timer");
   this.modalBody = document.querySelector(".modal-body");
+  this.timeLimit = 30;
 
   this.cardClickHandler = event => {
     if (event.target.tagName === "LI") {
@@ -28,32 +24,48 @@ function MemoryGame() {
     }
   };
 
-  this.restartButtonHandler = event => {
-    this.totalSeconds = 180;
-      this.timerElement.textContent = this.totalSeconds + ' second(s) remaining';
-    clearInterval(this.intervalHandler);
+  this.addStar = () => {
+    let li = document.createElement('li');
+    let i = document.createElement("i");
+    i.className = "fa fa-star";
+    li.appendChild(i);
+    this.starsContainer.appendChild(li);
+  }
 
+  //When any of the restart button is clicked, we're initializing all values to default.
+  this.restartButtonHandler = event => {
+    this.totalSeconds = this.timeLimit;
+    this.timerElement.textContent = this.totalSeconds + " second(s) remaining";
+    
+    //Making sure existing timer is cleared before setting a new one.
+    clearInterval(this.intervalHandler);
     this.intervalHandler = setInterval(() => {
       this.totalSeconds--;
-      this.timerElement.textContent = this.totalSeconds + ' second(s) remaining';
+      this.timerElement.textContent =
+        this.totalSeconds + " second(s) remaining";
       if (this.totalSeconds === 0) {
         if (this.matchCount < 8) {
-          this.finished(`Oops!! Time Up !!`);
+          this.finished(`Oops!! Time Up !!`, false);
         }
       }
     }, 1000);
 
+    //As the cards will be removed, we're clearning any existing event handlers registered.
     this.container.removeEventListener("click", this.cardClickHandler);
 
     this.modal.style.display = "none";
     this.modalbackDrop.style.display = "none";
-    this.starsContainer.innerHTML = this.starElements;
+    this.starsContainer.innerHTML = '';
+    this.addStar();
     this.starLimit = this.threeStarLimit;
     const moves = 0;
     this.moves = moves;
     this.updateMoves(moves);
     this.matchCount = 0;
     this.lastSelectedCard = { data: {} };
+
+    //After shuffling the data, we're returning object so we can add extra properties to it
+    //Instead of using the type of card and relying on the DOM
     this.cards = shuffle([
       "diamond",
       "paper-plane-o",
@@ -82,13 +94,10 @@ function MemoryGame() {
   };
 
   this.updateMoves = count => {
-    if (count > this.starLimit && this.starsContainer.children.length > 1) {
-      this.starsContainer.children[0].remove();
-      this.starLimit = this.twoStarLimit;
-    }
     this.movesElement.textContent = count;
   };
 
+   //Hides Modal fade and modal pop-up during initialization and aatach event lsiteners.
   this.init = () => {
     this.restartButtonHandler();
     document.getElementById("modal-close").addEventListener("click", event => {
@@ -100,6 +109,7 @@ function MemoryGame() {
     restartButtons[1].addEventListener("click", this.restartButtonHandler);
   };
 
+  //Create list of cards and put it inside the cards container and add event listener to it
   this.render = () => {
     this.container.innerHTML = "";
     let docFrag = document.createDocumentFragment();
@@ -124,8 +134,16 @@ function MemoryGame() {
     this.container.addEventListener("click", this.cardClickHandler);
   };
 
+  /**
+  * @description 
+  * Hide both cards that are currently being shown and release the click lock
+  * only after the visible cards are flipped.              
+  * @param {object} element1 - Current Selected Card
+  * @param {object} element2 - Last Selected Card 
+  */
   this.hide = (element1, element2) => {
     this.preventClick = true;
+    clearTimeout(this.timeoutCounter);
     this.timeoutCounter = setTimeout(() => {
       element1.setAttribute("class", "card");
       element2.setAttribute("class", "card");
@@ -133,8 +151,19 @@ function MemoryGame() {
     }, 500);
   };
 
-  this.finished = text => {
-    this.modalBody.innerHTML = `<h1>${text}</h1>`;
+  this.finished = (text, madeit) => {
+    let color = madeit ? "green" : "red";
+    let tempHtml = `<h1 style="color:${color}">${text}</h1>
+                    <hr>
+                    <h3>Time taken: ${this.timeLimit - this.totalSeconds} second(s)
+                    <h3>Total Moves: ${this.moves}</h3>
+                    <h3>Successful Match: ${this.matchCount}
+                    <h3>Your Rating</h3>
+                    <ul class="popup-stars">
+                    ${this.starsContainer.innerHTML}
+                  </ul>`;
+    
+    this.modalBody.innerHTML = tempHtml;
     this.modal.style.display = "block";
     this.modalbackDrop.style.display = "block";
     this.container.removeEventListener("click", this.cardClickHandler);
@@ -146,25 +175,36 @@ function MemoryGame() {
       return;
     }
 
+    //Check if user has selected the same card twice
+    //Also check if user selected a card that was already matched
     if (
       this.lastSelectedCard.data.index !== element.data.index ||
       (this.lastSelectedCard.data.match != true && element.data.match !== true)
     ) {
+
+      //If not flip the current card open.
       element.setAttribute("class", "card open show");
       element.data.open = true;
 
+      //If there's a card opened already than the current one
       if (this.lastSelectedCard.data.type) {
+
+        //Check if both of them matches or not
         if (element.data.type === this.lastSelectedCard.data.type) {
           element.setAttribute("class", "card match");
           this.lastSelectedCard.setAttribute("class", "card match");
           element.data.match = true;
           this.lastSelectedCard.match = true;
           this.matchCount++;
+          if (this.matchCount === 4 || this.matchCount === 8) {
+            this.addStar();
+          }
+          //Check if all the cards has been matched or not.
           if (this.matchCount === 8) {
-            this.finished(`You made it !!!!`);
+            this.finished(`You made it !!!!`, true);
           }
         } else {
-          clearTimeout(this.timeoutCounter);
+          //Updated values as false but flip the card only after a certain time
           this.hide(element, this.lastSelectedCard);
           element.data.open = false;
           this.lastSelectedCard.data.open = false;
